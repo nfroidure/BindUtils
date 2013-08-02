411dd2ed76754cf45253527963e3ff1fbea3c657
Nicolas Froidure <nicolas_froidure@elitwork.com>
2013-08-02 16:14:55 +0200

    Added a dev option

--- printzones.sh
+++ printzones.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 #
 # Reprint all zones of the domains saved
 #
@@ -8,8 +8,23 @@
 # Constants
 CONF_PATH="../conf/";
 
+# Params
+print=$1
+if [ "$print" = "" ]; then
+	echo "$(basename $0) 1:(dev|prod)"
+	exit
+fi
+
+echo "Emptying zone file"
+echo "//Here goes production zones;
+" >> /etc/bind/named.conf.production
+
 echo "Re-printing zones"
-while read username domain
+while read username domain devonly
 do
-	$(dirname $0)/printzone.sh $domain
-done < "$(dirname $0)/${CONF_PATH}domains.db"
+	if [ "$devonly" == "yes" -a "prod" == "$print" ]; then
+		echo "$domain : Skipping "
+	else
+		$(dirname $0)/printzone.sh $domain
+	fi
+done < $(dirname $0)/${CONF_PATH}domains.db
